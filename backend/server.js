cafb75e80bfb9bdd55f553bd567a1336ff86a5d2
ankur srivastava <careermailankur@gmail.com>
2021-12-25 12:05:24 +0530

    add database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const app = require('./app');
-
+const connectDatabase = require('./config/database')
 const dotenv = require('dotenv');
 
 dotenv.config({path:'backend/config/config.env'})
+
+connectDatabase();
 
 app.listen(process.env.PORT,()=>{
     console.log(`server is running on PORT:${process.env.PORT} in ${process.env.PORT_ENV} mode`)
